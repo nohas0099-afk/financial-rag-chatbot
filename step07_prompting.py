c472cb93e66c002d85bbdd5a3c288209a51911bc
nohas0099-afk <nohas0099@gmail.com>
2026-07-22 12:07:28 +0300

    Update step07_prompting.py

--- step07_prompting.py
+++ step07_prompting.py
@@ -6,25 +6,27 @@ from langchain.memory import ConversationBufferMemory
 
 
 def load_llm():
-    token = st.secrets.get("HUGGINGFACEHUB_API_TOKEN") or os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
-    return HuggingFaceEndpoint(
+    llm = HuggingFaceEndpoint(
         repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         huggingfacehub_api_token=token,
-        max_new_tokens=128,
         temperature=0.1,
+        max_new_tokens=128,
     )
+
+    return llm
 
 
 def create_rag_chain(llm, retriever):
     memory = ConversationBufferMemory(
         memory_key="chat_history",
         output_key="answer",
-        return_messages=True
+        return_messages=True,
     )
 
     return ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=retriever,
-        memory=memory
+        memory=memory,
     )
