8543801fc70f1de49f8f7cc9435662e118671d23
nohas0099-afk <nohas0099@gmail.com>
2026-07-22 12:45:51 +0300

    Update step04_vector_representation.py

--- step04_vector_representation.py
+++ step04_vector_representation.py
@@ -1,14 +1,21 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embedding_model():
     """Loads Hugging Face Sentence Transformers embeddings."""
-    model_path = "sentence-transformers/all-MiniLM-l6-v2"
-    model_kwargs = {'device': 'cpu'}
-    encode_kwargs = {'normalize_embeddings': False}
-    
-    hf = HuggingFaceEmbeddings(
+    model_path = "sentence-transformers/all-MiniLM-L6-v2"
+
+    model_kwargs = {
+        "device": "cpu"
+    }
+
+    encode_kwargs = {
+        "normalize_embeddings": False
+    }
+
+    embeddings = HuggingFaceEmbeddings(
         model_name=model_path,
         model_kwargs=model_kwargs,
-        encode_kwargs=encode_kwargs
+        encode_kwargs=encode_kwargs,
     )
-    return hf
+
+    return embeddings
